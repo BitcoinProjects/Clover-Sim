32801c11f1df80c4ec2dcbb492ef478b277c3d9b
Federico Franzoni <fedfranz.dev@outlook.com>
2021-05-27 19:11:30 +0200

    update

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python btcnetsim.py create 10 5 2 2 30 30 \
+python btcnetsim.py create clover 10 0 40 \
 && python firstspy.py run 2 1 \
 && python btcnetsim.py txinit \
 && python btcnetsim.py txrun 120 2
